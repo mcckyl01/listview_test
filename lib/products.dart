a38ec62516adfe9a92a626ff330515006ea453d6
mcckyl01 <mcckyl01@gmail.com>
2020-02-16 12:25:58 -0800

    second commit

---
+++ lib/products.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class Products extends StatelessWidget {
+  final List<String> products;
+  Products(this.products);
+
+  Widget _buildProductItem(BuildContext context, int index) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
+        
+        child: FlatButton(
+          onPressed: () {},
+                  child: Row(
+             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Image.asset(
+                'assets/blue.png',
+                width: 100,
+              ),
+              Column(
+                children: <Widget>[
+                  Text('Name: %name%',
+                  style: TextStyle(fontSize: 20.0),
+                  ),
+                  Text('Owner: %owner%',
+                  style: TextStyle(fontSize: 15.0),
+                  ),
+                  Text('Relay Type: %relay type%',
+                  style: TextStyle(fontSize: 15.0),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: _buildProductItem,
+      itemCount: products.length,
+    );
+  }
+}
